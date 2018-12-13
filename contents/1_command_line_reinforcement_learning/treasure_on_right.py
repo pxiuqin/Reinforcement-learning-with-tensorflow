dd62cceba33203ce330aa5daec76d2da8361b39f
liangxiuqin <liangxiuqin@mininglamp.com>
2018-12-13 19:38:06 +0800

    update q-learning

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -12,46 +12,46 @@ import time
 
 np.random.seed(2)  # reproducible
 
-
-N_STATES = 6   # the length of the 1 dimensional world
-ACTIONS = ['left', 'right']     # available actions
-EPSILON = 0.9   # greedy police 贪婪策略
-ALPHA = 0.1     # learning rate
-GAMMA = 0.9    # discount factor
-MAX_EPISODES = 13   # maximum episodes
-FRESH_TIME = 0.3    # fresh time for one move
+N_STATES = 6  # the length of the 1 dimensional world
+ACTIONS = ['left', 'right']  # available actions
+EPSILON = 0.9  # greedy police 贪婪策略
+ALPHA = 0.1  # learning rate
+GAMMA = 0.9  # discount factor
+MAX_EPISODES = 13  # maximum episodes
+FRESH_TIME = 0.3  # fresh time for one move
 
 
 def build_q_table(n_states, actions):
     table = pd.DataFrame(
-        np.zeros((n_states, len(actions))),     # q_table initial values  初始化为0
-        columns=actions,    # actions's name  指定了列名称
+            np.zeros((n_states, len(actions))),  # q_table initial values  初始化为0
+            columns=actions,  # actions's name  指定了列名称
     )
     # print(table)    # show table
     return table
 
 
-#选择一个动作
+# 选择一个动作
 def choose_action(state, q_table):
     # This is how to choose an action
     state_actions = q_table.iloc[state, :]
     if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):  # act non-greedy or state-action have no value
-        action_name = np.random.choice(ACTIONS)   #如果是开始就随机选择一个
-    else:   # act greedy
-        action_name = state_actions.idxmax()    # replace argmax to idxmax as argmax means a different function in newer version of pandas
+        action_name = np.random.choice(ACTIONS)  # 如果是开始就随机选择一个
+    else:  # act greedy
+        action_name = state_actions.idxmax()  # replace argmax to idxmax as argmax means a different function in newer version of pandas
     return action_name
 
-#给出的一个环境
+
+# 给出的一个环境
 def get_env_feedback(S, A):
     # This is how agent will interact with the environment
-    if A == 'right':    # move right
-        if S == N_STATES - 2:   # terminate
+    if A == 'right':  # move right
+        if S == N_STATES - 2:  # terminate
             S_ = 'terminal'
             R = 1
         else:
             S_ = S + 1
             R = 0
-    else:   # move left
+    else:  # move left
         R = 0
         if S == 0:
             S_ = S  # reach the wall  这里认为是一堵墙
@@ -62,9 +62,9 @@ def get_env_feedback(S, A):
 
 def update_env(S, episode, step_counter):
     # This is how environment be updated
-    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
+    env_list = ['-'] * (N_STATES - 1) + ['T']  # '---------T' our environment
     if S == 'terminal':
-        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+        interaction = 'Episode %s: total_steps = %s' % (episode + 1, step_counter)
         print('\r{}'.format(interaction), end='')
         time.sleep(2)
         print('\r                                ', end='')
@@ -72,46 +72,46 @@ def update_env(S, episode, step_counter):
         env_list[S] = 'o'
         interaction = ''.join(env_list)
         print('\r{}'.format(interaction), end='')
-        time.sleep(FRESH_TIME)   #停止一个刷新时间
+        time.sleep(FRESH_TIME)  # 停止一个刷新时间
 
 
 def rl():
     # main part of RL loop
-    q_table = build_q_table(N_STATES, ACTIONS)  #构建一个q_table
-    for episode in range(MAX_EPISODES):   #总共迭代次数
-        step_counter = 0   #状态遍历次数
-        S = 0  #初始状态
-        is_terminated = False   #是否完结
+    q_table = build_q_table(N_STATES, ACTIONS)  # 构建一个q_table
+    for episode in range(MAX_EPISODES):  # 总共迭代次数
+        step_counter = 0  # 状态遍历次数
+        S = 0  # 初始状态
+        is_terminated = False  # 是否完结
         update_env(S, episode, step_counter)
         while not is_terminated:
 
             A = choose_action(S, q_table)
             S_, R = get_env_feedback(S, A)  # take action & get next state and reward  获取下一状态和Reward
-            q_predict = q_table.loc[S, A]   #当前Q值
+            q_predict = q_table.loc[S, A]  # 当前Q值
             if S_ != 'terminal':
-                #Rt+1 + gamma * Q(St+1,A`)  获取下状态的最大值Q值
+                # Rt+1 + gamma * Q(St+1,A`)  获取下状态的最大值Q值
                 '''
                 这里特殊说明下为什么是Max
                 现在At+1~mju(.|St)  主要是把Q策略转换到A策略上，这是off-policy的核心思路
                 那么A`~PI(.|St)  表示找出来的最好策略
-                那么PI(St+1)=argmaxQ(St+1,a`)  根据贪婪策略这个为最好值
+                那么PI(St+1)=argmaxQ(St+1,a`)  根据贪婪策略这个为最好值,这里就把A`指定为a`了
                 所以Rt+1 + gamma * Q(St+1,A`)
                 =Rt+1 + gamma * Q(St+1,argmaxQ(St+1,a`))
                 =Rt+1 + gamma * maxQ(St+1,a`)   这里直接使用了最大值动作并且与当前策略即选取动作At时采用的策略无关
                 主要可以理解为maxQ(St+1,a`)记忆中的利益，表示在状态St+1中给出的最大效用，表示我要把这个最大效用提
                 前告诉给上一个状态，让他知道这个消息，这里gamma越大表示越重视以前的历史经验，越小表示只重视眼前利益
                 '''
-                q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
+                q_target = R + GAMMA * q_table.iloc[S_, :].max()  # next state is not terminal
             else:
-                q_target = R     # next state is terminal 如果是结束了则为Reward
-                is_terminated = True    # terminate this episode
+                q_target = R  # next state is terminal 如果是结束了则为Reward
+                is_terminated = True  # terminate this episode
 
-            #Q(St,At)=Q(St,At)+alpha* (Rt+1 + gamma * Q(St+1,A`)-Q(St,At))      （q_target-q_predict）
+            # Q(St,At)=Q(St,At)+alpha* (Rt+1 + gamma * Q(St+1,A`)-Q(St,At))      （q_target-q_predict）
             q_table.loc[S, A] += ALPHA * (q_target - q_predict)  # update
             S = S_  # move to next state   状态变换
 
-            update_env(S, episode, step_counter+1)   #更新环境（输出）
-            step_counter += 1  #步数+1
+            update_env(S, episode, step_counter + 1)  # 更新环境（输出）
+            step_counter += 1  # 步数+1
     return q_table
 
 
