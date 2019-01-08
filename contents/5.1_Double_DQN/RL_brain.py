870075cd15d7412d9c672e4559deb2e44d595f88
liangxiuqin <liangxiuqin@mininglamp.com>
2019-01-08 14:33:20 +0800

    update Double_DQN

--- contents/5.1_Double_DQN/RL_brain.py
+++ contents/5.1_Double_DQN/RL_brain.py
@@ -9,7 +9,14 @@ gym: 0.8.0
 
 Double_DQN:
 
-其他情况和DNQ一致
+其他情况和DNQ一致，原因是估值太高，需要通过两个网络来完成评估
+在标准的 Q-学习和 DQN 中的 max 操作使用同样的值来进行选择和衡量一个行动。
+这实际上更可能选择过高的估计值，从而导致过于乐观的值估计。
+为了避免这种情况的出现，我们可以对选择和衡量进行解耦。
+
+最初的双 Q-学习算法中，两个值函数通过将每个经验随机更新两个值函数中的一个，这样就出现了两个权重集合，θ 和 θ'。
+对每个更新，一个权重集合用来确定贪心策略，另一个用来确定值。
+为了更好地比较这两者，我们可以将 Q-学习中的选择和衡量分解
 """
 
 import numpy as np
@@ -127,23 +134,24 @@ class DoubleDQN:
             self.sess.run(self.replace_target_op)
             print('\ntarget_params_replaced\n')
 
+        # 内存大小设置上限
         if self.memory_counter > self.memory_size:
             sample_index = np.random.choice(self.memory_size, size=self.batch_size)
         else:
             sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
-        batch_memory = self.memory[sample_index, :]
+        batch_memory = self.memory[sample_index, :]   #给定经验记忆
 
         # 这里需要计算q_eval4next是根据经验池中下一时刻状态s'输入到eval-net计算得到的q值，这个q值主要用来选择动作
         q_next, q_eval4next = self.sess.run(
             [self.q_next, self.q_eval],
             feed_dict={self.s_: batch_memory[:, -self.n_features:],    # next observation
                        self.s: batch_memory[:, -self.n_features:]})    # next observation
-        q_eval = self.sess.run(self.q_eval, {self.s: batch_memory[:, :self.n_features]})
+        q_eval = self.sess.run(self.q_eval, {self.s: batch_memory[:, :self.n_features]})  #计算一个经验值q_eval的值
 
         q_target = q_eval.copy()  #赋值
 
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
-        eval_act_index = batch_memory[:, self.n_features].astype(int)
+        batch_index = np.arange(self.batch_size, dtype=np.int32)  #批量大小
+        eval_act_index = batch_memory[:, self.n_features].astype(int)   #给定动作指针
         reward = batch_memory[:, self.n_features + 1]
 
         # 如果是double Q的话需要根据刚刚计算的q_eval4next来选择动作，然后根据q_next来得到q值的
@@ -154,11 +162,11 @@ class DoubleDQN:
         else:
             selected_q_next = np.max(q_next, axis=1)    # the natural DQN
 
-        q_target[batch_index, eval_act_index] = reward + self.gamma * selected_q_next
+        q_target[batch_index, eval_act_index] = reward + self.gamma * selected_q_next   #给定q_target的值，选择和衡量分开
 
         _, self.cost = self.sess.run([self._train_op, self.loss],
                                      feed_dict={self.s: batch_memory[:, :self.n_features],
-                                                self.q_target: q_target})
+                                                self.q_target: q_target})   #给定了新的q_target来训练网络，从而得出
         self.cost_his.append(self.cost)
 
         self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
