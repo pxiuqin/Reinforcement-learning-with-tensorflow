d7fc1e8bf05b86be9c28eba00bb759d9b450db8d
liangxiuqin <liangxiuqin@mininglamp.com>
2019-01-31 14:47:37 +0800

    update a3c

--- contents/10_A3C/A3C_distributed_tf.py
+++ contents/10_A3C/A3C_distributed_tf.py
@@ -5,6 +5,8 @@ The Cartpole example using distributed tensorflow + multiprocessing.
 
 View more on my tutorial page: https://morvanzhou.github.io/
 
+使用multiprocessing，分布式实现A3C
+
 """
 
 import multiprocessing as mp
@@ -29,34 +31,35 @@ class ACNet(object):
     sess = None
 
     def __init__(self, scope, opt_a=None, opt_c=None, global_net=None):
+        #判断是否为global_net
         if scope == 'global_net':  # get global network
             with tf.variable_scope(scope):
                 self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
                 self.a_params, self.c_params = self._build_net(scope)[-2:]
         else:
-            with tf.variable_scope(scope):
-                self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
-                self.a_his = tf.placeholder(tf.int32, [None, ], 'A')
+            with tf.variable_scope(scope):  #本地任务是那个worker
+                self.s = tf.placeholder(tf.float32, [None, N_S], 'S')   #表示状态
+                self.a_his = tf.placeholder(tf.int32, [None, ], 'A')    #表示历史动作
                 self.v_target = tf.placeholder(tf.float32, [None, 1], 'Vtarget')
 
-                self.a_prob, self.v, self.a_params, self.c_params = self._build_net(scope)
+                self.a_prob, self.v, self.a_params, self.c_params = self._build_net(scope)   #构建本地网络
 
-                td = tf.subtract(self.v_target, self.v, name='TD_error')
+                td = tf.subtract(self.v_target, self.v, name='TD_error')  #获取td_error
                 with tf.name_scope('c_loss'):
                     self.c_loss = tf.reduce_mean(tf.square(td))
 
                 with tf.name_scope('a_loss'):
                     log_prob = tf.reduce_sum(
                         tf.log(self.a_prob) * tf.one_hot(self.a_his, N_A, dtype=tf.float32),
-                        axis=1, keep_dims=True)
-                    exp_v = log_prob * tf.stop_gradient(td)
+                        axis=1, keep_dims=True)  #给出概率密度函数
+                    exp_v = log_prob * tf.stop_gradient(td)  #停止点的期望值
                     entropy = -tf.reduce_sum(self.a_prob * tf.log(self.a_prob + 1e-5),
-                                             axis=1, keep_dims=True)  # encourage exploration
-                    self.exp_v = ENTROPY_BETA * entropy + exp_v
-                    self.a_loss = tf.reduce_mean(-self.exp_v)
+                                             axis=1, keep_dims=True)  # encourage exploration  计算熵值
+                    self.exp_v = ENTROPY_BETA * entropy + exp_v   #联合计算出期望值
+                    self.a_loss = tf.reduce_mean(-self.exp_v)   #计算损失
 
                 with tf.name_scope('local_grad'):
-                    self.a_grads = tf.gradients(self.a_loss, self.a_params)
+                    self.a_grads = tf.gradients(self.a_loss, self.a_params)   #计算梯度
                     self.c_grads = tf.gradients(self.c_loss, self.c_params)
 
             self.global_step = tf.train.get_or_create_global_step()
@@ -69,26 +72,29 @@ class ACNet(object):
                     self.update_c_op = opt_c.apply_gradients(zip(self.c_grads, global_net.c_params))
 
     def _build_net(self, scope):
-        w_init = tf.random_normal_initializer(0., .1)
+        w_init = tf.random_normal_initializer(0., .1)  #用来初始化权重
         with tf.variable_scope('actor'):
+            #设置一个200维度的输出
             l_a = tf.layers.dense(self.s, 200, tf.nn.relu6, kernel_initializer=w_init, name='la')
             a_prob = tf.layers.dense(l_a, N_A, tf.nn.softmax, kernel_initializer=w_init, name='ap')
         with tf.variable_scope('critic'):
             l_c = tf.layers.dense(self.s, 100, tf.nn.relu6, kernel_initializer=w_init, name='lc')
             v = tf.layers.dense(l_c, 1, kernel_initializer=w_init, name='v')  # state value
-        a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
-        c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic')
+        a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')  #actor参数
+        c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic') #critic参数
         return a_prob, v, a_params, c_params
 
     def choose_action(self, s):  # run by a local
-        prob_weights = self.sess.run(self.a_prob, feed_dict={self.s: s[np.newaxis, :]})
+        prob_weights = self.sess.run(self.a_prob, feed_dict={self.s: s[np.newaxis, :]})   #获取权重
         action = np.random.choice(range(prob_weights.shape[1]),
-                                  p=prob_weights.ravel())  # select action w.r.t the actions prob
+                                  p=prob_weights.ravel())  # select action w.r.t the actions prob 选择动作
         return action
 
+    #更新global_net参数
     def update_global(self, feed_dict):  # run by a local
         self.sess.run([self.update_a_op, self.update_c_op], feed_dict)  # local grads applies to global net
 
+    #获取global_net参数
     def pull_global(self):  # run by a local
         self.sess.run([self.pull_a_params_op, self.pull_c_params_op])
 
@@ -105,7 +111,7 @@ def work(job_name, task_index, global_ep, lock, r_queue, global_running_r):
         server.join()
     else:
         t1 = time.time()
-        env = gym.make('CartPole-v0').unwrapped
+        env = gym.make('CartPole-v0').unwrapped   #游戏环境
         print('Start Worker: ', task_index)
         with tf.device(tf.train.replica_device_setter(
                 worker_device="/job:worker/task:%d" % task_index,
@@ -114,7 +120,7 @@ def work(job_name, task_index, global_ep, lock, r_queue, global_running_r):
             opt_c = tf.train.RMSPropOptimizer(LR_C, name='opt_c')
             global_net = ACNet('global_net')
 
-        local_net = ACNet('local_ac%d' % task_index, opt_a, opt_c, global_net)
+        local_net = ACNet('local_ac%d' % task_index, opt_a, opt_c, global_net)  #构建本地网络
         # set training steps
         hooks = [tf.train.StopAtStepHook(last_step=100000)]
         with tf.train.MonitoredTrainingSession(master=server.target,
@@ -130,27 +136,28 @@ def work(job_name, task_index, global_ep, lock, r_queue, global_running_r):
                 while True:
                     # if task_index:
                     #     env.render()
-                    a = local_net.choose_action(s)
-                    s_, r, done, info = env.step(a)
+                    a = local_net.choose_action(s)  #本地网络选择动作
+                    s_, r, done, info = env.step(a)  #和环境交互
                     if done: r = -5.
                     ep_r += r
-                    buffer_s.append(s)
-                    buffer_a.append(a)
-                    buffer_r.append(r)
+                    buffer_s.append(s)  #状态缓存
+                    buffer_a.append(a)  #动作缓存
+                    buffer_r.append(r)  #奖励缓存
 
                     if total_step % UPDATE_GLOBAL_ITER == 0 or done:  # update global and assign to local net
                         if done:
                             v_s_ = 0  # terminal
                         else:
+                            #获取新value值
                             v_s_ = sess.run(local_net.v, {local_net.s: s_[np.newaxis, :]})[0, 0]
                         buffer_v_target = []
                         for r in buffer_r[::-1]:  # reverse buffer r
-                            v_s_ = r + GAMMA * v_s_
+                            v_s_ = r + GAMMA * v_s_   #完成新的value值
                             buffer_v_target.append(v_s_)
                         buffer_v_target.reverse()
 
                         buffer_s, buffer_a, buffer_v_target = np.vstack(buffer_s), np.array(buffer_a), np.vstack(
-                            buffer_v_target)
+                            buffer_v_target)  #转换
                         feed_dict = {
                             local_net.s: buffer_s,
                             local_net.a_his: buffer_a,
@@ -165,7 +172,7 @@ def work(job_name, task_index, global_ep, lock, r_queue, global_running_r):
                         if r_queue.empty():  # record running episode reward
                             global_running_r.value = ep_r
                         else:
-                            global_running_r.value = .99 * global_running_r.value + 0.01 * ep_r
+                            global_running_r.value = .99 * global_running_r.value + 0.01 * ep_r   #全局奖励
                         r_queue.put(global_running_r.value)
 
                         print(
